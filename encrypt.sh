ccf963e6e59eee3b605e71fbfc70e6eaa84b07ea
Joshua Haupt <joshua@hauptj.com>
2018-06-17 21:53:41 -0500

    added circular deployment

--- encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Run in Ansible directory
-sudo su
-ansible-vault encrypt group_vars/site/vault.yml --vault-password-file ~/vault_test.txt
-ansible-vault encrypt group_vars/wordpress/vault.yml --vault-password-file ~/vault_test.txt
-su vagrant
